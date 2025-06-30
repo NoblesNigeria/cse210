8951d68cc5b59aa959843228dff97d95444cb341
NoblesNigeria <noblesnigeria@gmail.com>
2025-06-30 14:14:20 +0100

    I added my full name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project of Anthony.");
+        Console.WriteLine("Hello World! tell me your name.");
+        Console.WriteLine("Enter your first name: ");
+        Console.WriteLine("Enter your last name: ");
+        Console.WriteLine("Enter your middle name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello {name}, welcome to CSE 210!");
+        Console.WriteLine("This is the first exercise of the course.");
     }
 }
