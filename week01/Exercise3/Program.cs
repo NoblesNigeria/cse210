8cd53053acf43a2e76516fdc05a868ce3dd02135
NoblesNigeria <noblesnigeria@gmail.com>
2025-07-02 17:03:05 +0100

    word guess game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string magicWord = "";
+        string guessWord = "";
+        int attemptsCounts = 0;
+        int maxAttempts = 5;
+        Console.WriteLine("Please enter a magic word:");
+        magicWord = Console.ReadLine();
+        guessWord = Console.ReadLine();
+        while (guessWord != magicWord && attemptsCounts < maxAttempts)
+        {
+            Console.WriteLine("Incorrect guess. Please try again.");
+            attemptsCounts++;
+            if (attemptsCounts < maxAttempts)
+            {
+                guessWord = Console.ReadLine();
+            }
+            else if (attemptsCounts == maxAttempts)
+            {
+                Console.WriteLine("You have used all your attempts. The magic word was: " + magicWord);
+            }
+        }
+        if (guessWord == magicWord)
+            {
+                Console.WriteLine("Congratulations! You guessed the magic word: " + magicWord + ", in " + attemptsCounts + " attempts.");
+            }
+
     }
 }
