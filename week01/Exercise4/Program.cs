dbeb8db8f08c096bbb7bf3f7ea685b9b1c8c23e0
NoblesNigeria <noblesnigeria@gmail.com>
2025-07-05 17:33:58 +0100

    list

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number = 0;
+        float average = 0;
+        Console.WriteLine("Please enter a number (1 to stop):");
+        number = int.Parse(Console.ReadLine());
+        while (number != 0)
+        {
+            Console.WriteLine("Please enter a number (1 to stop):");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+            numbers.Count();
+        }
+        if (numbers.Count() > 0)
+        {
+            int sum =+number;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            average = sum / numbers.Count();
+            Console.WriteLine("The average of the entered numbers is: " + average);
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
+        {
+            Console.WriteLine(numbers);
+        }
+
+
+
     }
 }
