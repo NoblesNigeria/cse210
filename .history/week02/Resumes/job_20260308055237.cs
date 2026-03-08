00644f4db74b1ad9df7b76b482ff97f29e15f9a6
NoblesNigeria <noblesnigeria@gmail.com>
2026-03-08 05:53:02 +0000

    checked

---
+++ .history/week02/Resumes/job_20260308055237.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+    Publi
+    {
+        _company = "Microsoft";
+        _jobTitle = "Software Engineer";
+        _startYear = 2020;
+        _endYear = 2023;
+    }
+    public void Display()
+    {
+        Console.WriteLine($"Company: {_company}");
+        Console.WriteLine($"Job Title: {_jobTitle}");
+        Console.WriteLine($"Start Year: {_startYear}");
+        Console.WriteLine($"End Year: {_endYear}");
+    }
+}
